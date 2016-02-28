856e3128a549d9fb4f88fadc97040a306c4b47ce
CMB <418328802@qq.com>
2016-02-28 14:00:42 +0800

    alter ignore

--- admin.php
+++ admin.php
@@ -4,7 +4,9 @@
 	//确定应用路径
 	define('APP_PATH','./Admin/');
 	//开启调试模式
-	define('APP_DEBUG',TRUE);
+	define('APP_DEBUG',FALSE);
+	// 定义运行时目录
+	define('RUNTIME_PATH', './Runtime/Admin/');
 	//确定核心文件
 	require'./ThinkPHP/ThinkPHP.php';
 ?>
--- index.php
+++ index.php
@@ -3,8 +3,10 @@
 	define('APP_NAME','./SCDA');
 	//确定应用路径
 	define('APP_PATH','./SCDA/');
-	//
-	define('APP_DEBUG',TRUE);
+	// Debug模式
+	define('APP_DEBUG',FALSE);
+	// 定义运行时目录
+	define('RUNTIME_PATH', './Runtime/Index/');
 	//确定核心文件
 	require'./ThinkPHP/ThinkPHP.php';
 ?>
